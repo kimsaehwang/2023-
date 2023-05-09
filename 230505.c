c1c5fd0aa1863059993f6be8de06062415055dd2
kimsaehwang <131853452+kimsaehwang@users.noreply.github.com>
2023-05-09 23:12:28 +0900

    Add files via upload

--- 230505.c
+++ 230505.c
@@ -4,82 +4,171 @@
 #include <string.h>
 #include <windows.h>
 
+//void input_nums(int* lotto_nums);
+//void print_nums(int* lotto_nums);
+//void check_duplication(int* lotto_nums, int* i);
+
 int main()
 {
-	//문자열 뒤집기 
-	char str[100];
-	int len;
-	gets(str);
-	len = strlen(str);//문자열 길이갯수를 str에 저장
-	char* front = &str[0];
-	char* back = ;
-
-
-	char temp;
-	for (int i = 0; i < len / 2; i++)
+	// 길이가 가장 긴 단어
+	/*
+	char ch;
+	int count = 0;
+	int temp = 0;
+	while ((scanf("%c", &ch)) != -1) // Ctrl+Z가 입력시 종료
 	{
-		temp = *(front + i);
-		*(front + i) = *(back - i);
-		*(back - i) = temp;
+		if (ch != '\n')
+		{
+			count++;
+		}
+		else
+		{
+			if (temp < count)
+			{ 
+				temp = count;
+				count = 0;
+			}
+		}
 	}
-	for (int i = 0; i <= len; i++)
+	printf("가장 긴 단어의 길이 : %d", temp);
+	return 0;
+}
+*/
+
+	//로또 번호생성기(함수)
+	/*
+	int lotto_nums[6];
+
+	input_nums(lotto_nums);
+	print_nums(lotto_nums);
+
+	return 0;
+}
+
+void input_nums(int* lotto_nums)
+{
+	for (int i = 0; i < 6; i++)
 	{
-		puts(str);
+		printf("번호 입력 : ");
+		scanf("%d", &lotto_nums[i]);
+		if(check_range(lotto_nums, &i))
+		{
+			continue;
+		}
+		check_duplication(lotto_nums, &i);
+	}
+}
+
+int check_range(int* lotto_nums, int* i)
+{
+	if (*(lotto_nums + *i) <= 0 || *(lotto_nums + *i) > 45)
+	{
+		printf("1~45숫자를 입력해주세요!\n");
+		(*i)--;
 	}
 	return 0;
 }
 
+void check_duplication(int* lotto_nums, int* i)
+{
+	for (int j = 0; j < *i; j++)
+	{
+		if (*(lotto_nums + *i) == *(lotto_nums + j))
+		{
+			printf("같은 번호를 입력 하셨습니다!\n");
+			(*i)--;
+		}
+	}
+}
 
-//숫자뒤집기
-/*
-int arr[6] = { 1,2,3,4,5,6 };
-int* front = &arr[0];
-int* back = &arr[5];
-int temp;
-for (int i = 0; i < 3; i++)
+void print_nums(int* lotto_nums)
 {
-	temp = *(front+i);
-	*(front + i) = *(back - i);
-	*(back - i) = temp;
-}
-for (int i = 0; i <= 5; i++)
-{
-	printf("%d", arr[i]);
-}
-return 0;
+	for (int i = 0; i < 6; i++)
+	{
+		printf("%3d", lotto_nums[i]);
+	}
 }
 */
 
+	//문자열 뒤집기 
+	/*char str[100];
+	char* front = str;
+	int i = 0;
+	gets(str);
 
-
-/*
-//포인터를 활용한 광고판
-char str[100];
-int size = sizeof(str) / sizeof(*str);
-printf("문자열 입력  -> ");
-gets(str);
-while (1)
-{
-	int temp;
-	temp = *str;
-	for (int i = 1;i < size;i++)
+	int len = strlen(str);     //문자열 길이갯수를 str에 저장
+	char* back = str + len -1; //전체길이 + len - 1을 하면 마지막자리
+	
+	for (i = 0; i < len; i++)
 	{
-		if (*(str+i) == '\0')
+		if (str[i] == '\0')
 		{
-			*(str+i - 1) = temp;
 			break;
 		}
-		*(str + i - 1) = *(str + i);
 	}
+
+	char temp;
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = *(front + i);
+		*(front + i) = *(back - i);
+		*(back - i) = temp;
+	} 
+
 	puts(str);
-	Sleep(500);
-	system("cls");
-}
-return 0;
+	return 0;
+}*/
+
+	//숫자뒤집기
+	/*
+	int arr[6] = { 1,2,3,4,5,6 };
+	int* front = &arr[0];
+	int* back = &arr[5];
+
+	int temp;
+
+	for (int i = 0; i < 3; i++)
+	{
+		temp = *(front+i);
+		*(front + i) = *(back - i);
+		*(back - i) = temp;
+	}
+
+	for (int i = 0; i <= 5; i++)
+	{
+		printf("%d", arr[i]);
+	}
+	return 0;
 }
 */
 
-/*
+	//포인터를 활용한 광고판
+	/*char str[100];
+	int size = sizeof(str) / sizeof(*str);
+	printf("문자열 입력  -> ");
+	gets(str);
+	while (1)
+	{
+		int temp;
+		temp = *str;
+		for (int i = 1;i < size;i++)
+		{
+			if (*(str+i) == '\0')
+			{
+				*(str+i - 1) = temp;
+				break;
+			}
+			*(str + i - 1) = *(str + i);
+		}
+		puts(str);
+		Sleep(500);
+		system("cls");
+	}
+	return 0;
+}
+*/
+
+	/*
 int ary[5] = { 10,20,30,40,50 };
 int* pa = ary;
 int* pb = pa + 3;
@@ -92,9 +181,9 @@ if (pa < pb)printf("%d\n", *pb);
 return 0;
 */
 
-/*
-//배열명에 정수 연산을 수행하여 배열 요소 사용
-int ary[3];
+
+	//배열명에 정수 연산을 수행하여 배열 요소 사용
+	/*int ary[3];
 int i;
 *(ary + 0) = 10;                   //ary[0] = 10;
 *(ary + 1) = *(ary + 0) + 10;      //ary[1] = ary[0] +10
