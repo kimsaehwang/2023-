2220c1f29e3764cd538943d518db3c99b5738152
kimsaehwang <131853452+kimsaehwang@users.noreply.github.com>
2023-05-19 17:42:22 +0900

    Add files via upload

--- Game.c
+++ Game.c
@@ -1,10 +1,8 @@
-﻿
-int k;
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h>
-
+#include <windows.h>﻿
 #define UP 0
 #define DOWN 1
 #define LEFT 2
@@ -14,11 +12,29 @@ int k;
 int KeyControl();
 void init();
 void Title();
+int omok();
 int menudraw();
 void infoDraw();
-void omokDraw();
+int omokDraw();
 void gotoxy(int x, int y);
 
+enum
+{
+	YELLOW, VIOLET,GREEN
+};
+
+void setColor(unsigned short text) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), text);
+}
+
+void CursorView()
+{
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+	cursorInfo.dwSize = 1; //커서 굵기 (1 ~ 100)
+	cursorInfo.bVisible = 0; //커서 Visible TRUE(보임) FALSE(숨김)
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+
+}
 
 int main()
 {
@@ -29,6 +45,19 @@ int main()
 		if (menucode == 0)
 		{
 			omokDraw();
+			int omokcode = omokDraw();
+			if (omokcode == 0)
+			{
+				omok();
+			}
+			else if (menucode == 1)
+			{
+				return 0;
+			}
+			else if (menucode == 2)
+			{
+				return 0;
+			}
 		}
 		else if (menucode == 1)
 		{
@@ -46,6 +75,7 @@ int main()
 
 void Title()
 {
+	setColor(GREEN);
 	printf("\n\n");
 	printf("           #####  #     #  #####  #    #       \n");
 	printf("          #     # ##   ## #     # #   #        \n");
@@ -54,7 +84,7 @@ void Title()
 	printf("          #     # #     # #     # #  #         \n");
 	printf("          #     # #     # #     # #   #        \n");
 	printf("           #####  #     #  #####  #    #       \n");
-
+	
 }
 
 void gotoxy(int x, int y)
@@ -131,13 +161,14 @@ void infoDraw()
 	printf("           돌 놓 기 : 스 페 이 스 바\n");
 	printf("        승 리 조 건 : 같은색 5개가 이어지면 이김\n\n");
 	printf("           spacebar 누르면 돌아감");
+	printf("           제 작 자 : 김 세 황");
 	while (1)
 	{
 		if (KeyControl() == SUBMIT) break;
 	}
 }
 
-void omokDraw()
+int omokDraw()
 {
 	int x = 20;
 	int y = 6;
@@ -181,6 +212,87 @@ void omokDraw()
 		case SUBMIT:
 		{
 			return y - 6;
+		}
+		}
+	}
+}
+
+int omok()
+{
+	system("cls");
+	printf("\n\n");
+	
+	printf("┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐\n");
+	for (int i = 0;i < 18;i++)
+	{
+		printf("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤\n");
+	}
+	printf("└─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘\n\n");
+
+	
+
+	//printf("○ : %d  ● : %d", );
+	int x = 38;     //0~38 짝수만
+	int y = 2;      //2~21
+	int m = 2;
+	gotoxy(x , y);
+	while (1)
+	{
+		int cnt;
+		int n = KeyControl();
+		switch (n) {
+		case UP:
+		{
+			if (y > 2)
+			{
+				gotoxy(x - 2, y);
+				//printf(" ");
+				gotoxy(x - 2, --y);
+
+			}
+			break;
+		}
+
+		case DOWN:
+		{
+			if (y < 21)
+			{
+				gotoxy(x - 2, y);
+				//printf(" ");
+				gotoxy(x - 2, ++y);
+			}
+			break;
+		}
+
+		case LEFT:
+		{
+			if (x > 0)
+			{
+				gotoxy(x, y);
+				//printf(" ");
+				x -= 2;
+				gotoxy(x, y);
+			}
+			break;
+		}
+
+		case RIGHT:
+		{
+			if (x < 38)
+			{
+				gotoxy(x, y);
+				//printf(" ");
+				x += 2;
+				gotoxy(x, y);
+			}
+			break;
+		}
+
+		case SUBMIT:
+		{
+			
+			printf("●");
+		
 		}
 		}
 	}
